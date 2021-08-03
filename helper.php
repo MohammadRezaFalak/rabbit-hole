a362ac6bd51fa662d0a81df592bb2beff49a8340
MohammadReza <m.reza6920@gmail.com>
2021-08-03 08:50:11 +0430

    provide upload path helper

---
+++ helper.php
@@ -0,0 +1,32 @@
+<?php
+
+function makeDirectory($path)
+{
+    if (!file_exists($path)) {
+        mkdir($path);
+    }
+}
+
+function makeUploadPath()
+{
+
+    $year = date("Y");
+    $month = date("m");
+    $day = date("d");
+
+    $arg = "./$year";
+
+    makeDirectory($arg);
+
+    $arg .= "/$month";
+
+    makeDirectory($arg);
+
+    $arg .= "/$day";
+
+    makeDirectory($arg);
+
+    return $arg;
+}
+
+echo makeUploadPath();
